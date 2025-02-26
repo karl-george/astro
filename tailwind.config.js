42dcf3bd7cf618e46d37e3b7b9e6d78d091fc50c
Karl George <karlgeorge87@gmail.com>
2025-02-26 16:37:26 +0100

    Update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       colors: {
         bg: '#060807',
-        accent: '#89CE87',
+        accent: '#C7F579',
         g1: '#060807',
         g2: '#078054',
         g3: '#0A8A86',
@@ -13,10 +13,22 @@ export default {
         t1: '#F3F3F3',
         t2: '#14190F',
         t3: '#989898',
+        buttonGreen: '#02221D',
+        buttonBlack: '#070909',
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
+      },
+
+      // Button Animation
+      animation: {
+        border: 'border 4s linear infinite',
+      },
+      keyframes: {
+        border: {
+          to: { '--border-angle': '360deg' },
+        },
       },
     },
   },
