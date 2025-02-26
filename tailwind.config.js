9330a33df39eeedc1f63e76b42209d63257cdf93
Karl George <karlgeorge87@gmail.com>
2025-02-26 11:45:09 +0100

    Add some basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: '#060807',
+        accent: '#89CE87',
+        g1: '#060807',
+        g2: '#078054',
+        g3: '#0A8A86',
+        g4: '#C0D4A1',
+        t1: '#F3F3F3',
+        t2: '#14190F',
+        t3: '#989898',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
