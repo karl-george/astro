a8527ea7b4255df60a10e3b1b8cb9229e28e39c9
Karl George <karlgeorge87@gmail.com>
2025-02-26 16:39:21 +0100

    Add hover effect

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ children, doesGlow, containerClasName }) => {
   {
     if (doesGlow) {
       return (
-        <button className='[background:linear-gradient(45deg,#03231E,#0A3D2D)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.green.600/.48)_80%,_theme(colors.lime.500)_86%,_theme(colors.lime.300)_90%,_theme(colors.lime.500)_94%,_theme(colors.green.600/.48))_border-box] rounded-3xl border border-transparent animate-border px-6 py-2 text-t3'>
+        <button className='[background:linear-gradient(45deg,#03231E,#0A3D2D)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.green.600/.48)_80%,_theme(colors.lime.500)_86%,_theme(colors.lime.300)_90%,_theme(colors.lime.500)_94%,_theme(colors.green.600/.48))_border-box] rounded-3xl border border-transparent animate-border px-6 py-2 text-t3 hover:bg-accent hover:transition-all hover:duration-500'>
           {children}
         </button>
       );
